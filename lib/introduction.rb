227064edbe6a8aba72249545a88bab07f453ddf7
Solangel Lagares <sp.lagares31@gmail.com>
2020-09-10 02:15:09 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,6 +4,6 @@ def introduction (name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction_with_language (Josh, Ruby)
-  puts "#{name} and I am learning to program in #{lang}."
+def introduction_with_language (name, language)
+  puts "#{name} and I am learning to program in #{language}."
 end 
